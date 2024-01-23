40ec2b79c3f224cde6abd9816418b26d569bab32
Astropeda <152017512+Astropeda@users.noreply.github.com>
2024-01-23 17:50:51 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,49 +3,49 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER || "2348039607375";
-global.mongodb = process.env.MONGODB_URI || "";
+global.owner = process.env.OWNER_NUMBER.split(",")
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
-global.audio = '' ; 
-global.video = '' ;
-global.email = 'astromedia0010@outlook.com';
-global.github = 'https://github.com/Astropeda/Asta-Md';
-global.location = 'Sultanpur IN';
-global.gurl = 'https://instagram.com/';
-global.timezone  = process.env.TIME_ZONE || 'Africa/Lagos'
-global.sudo = process.env.SUDO || '2348039607375';
-global.devs = '2348039607375';
-global.website = 'https://github.com/Astropeda/Asta-Md';
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://leadier-umbrellas.000webhostapp.com/botpic.jpeg';
+global.email = 'sam@secktor.live'
+global.github = 'https://github.com/SamPandey001/Secktor-Md'
+global.location = 'Sultanpur IN'
+global.gurl = 'https://instagram.com/' // add your username
+global.sudo = process.env.SUDO || '919628516236'
+global.devs = '919628516236';
+global.website = 'https://github.com/SamPandey001/Secktor-Md' //wa.me/+91000000000000
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png'
 module.exports = {
-  botname:   process.env.BOT_NAME || '',
-  ownername: process.env.OWNER_NAME || '',
-  sessionName:  process.env.SESSION_ID || '',
-  botbgm: process.env.BOT_BGM || 'false',
-  author:  process.env.PACK_INFO || 'Astro',
-  auto_read_status :  process.env.AUTO_READ_STATUS || 'false',
-  packname:  process.env.PACK_INFO || 'Astro',
-  autoreaction:  process.env.AUTO_REACTION  || 'false',
-  antibadword :  process.env.ANTI_BAD_WORD || 'nobadword',
-  alwaysonline:  process.env.ALWAYS_ONLINE || 'false',
-  antifake : process.env.FAKE_COUNTRY_CODE || '212',
-  readmessage:  process.env.READ_MESSAGE || 'false',
-  auto_status_saver: process.env.AUTO_STATUS_SAVER || 'false',
-  HANDLERS:  process.env.PREFIX || '.',
-  warncount : process.env.WARN_COUNT || "3",
-  disablepm:  process.env.DISABLE_PM || "false",
-  levelupmessage:  process.env.LEVEL_UP_MESSAGE || "false",
-  antilink:  process.env.ANTILINK_VALUES || 'chat.whatsapp.com',
-  antilinkaction: process.env.ANTILINK_ACTION || 'remove',
-  BRANCH: process.env.BRANCH || 'main',
-  ALIVE_MESSAGE:  process.env.ALIVE_MESSAGE || '@user i am alive',
-  autobio:  process.env.AUTO_BIO || 'false',
-  OPENAI_API_KEY:  process.env.OPENAI_API_KEY || 'null',
-  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
-  HEROKU_API_KEY: process.env.HEROKU_API_KEY,
-  VERSION: process.env.VERSION || 'v.0.0.3',
-  LANG: process.env.THEME || 'SECKTOR',
-  WORKTYPE: process.env.WORKTYPE || process.env.MODE || 'private'
+  botname:   process.env.BOT_NAME === undefined ? '𝐒𝐞𝐜𝐤𝐭𝐨𝐫 𝐁𝐨𝐭𝐭𝐨' : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? 'SamPandey001' : process.env.OWNER_NAME,
+  sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
+  author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'SamPandey001' : process.env.PACK_INFO.split(";")[0],
+  auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
+  packname:  process.env.PACK_INFO.split(";")[1] === undefined ? 'Secktor-Md' : process.env.PACK_INFO.split(";")[1],
+  autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
+  antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
+  alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
+  antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '971' : process.env.FAKE_COUNTRY_CODE,
+  readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE,
+  auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER,
+  HANDLERS:  process.env.PREFIX === undefined ? ['.'] : process.env.PREFIX,
+  warncount : process.env.WARN_COUNT === undefined ? 3 : process.env.WARN_COUNT,
+  disablepm:  process.env.DISABLE_PM === undefined ? false : process.env.DISABLE_PM,
+  levelupmessage:  process.env.LEVEL_UP_MESSAGE === undefined ? false : process.env.LEVEL_UP_MESSAGE,
+  antilink:  process.env.ANTILINK_VALUES === undefined ? 'chat.whatsapp.com' : process.env.ANTILINK_VALUES,
+  antilinkaction: process.env.ANTILINK_ACTION === undefined ? 'remove' : process.env.ANTILINK_ACTION,
+  BRANCH: 'main', 
+  ALIVE_MESSAGE:  process.env.ALIVE_MESSAGE === undefined ? '' : process.env.ALIVE_MESSAGE,
+  autobio:  process.env.AUTO_BIO === undefined ? false : process.env.AUTO_BIO,
+  OPENAI_API_KEY:  process.env.OPENAI_API_KEY === undefined ? false : process.env.OPENAI_API_KEY,
+  heroku:  process.env.heroku === undefined ? false : process.env.heroku,
+  HEROKU: {
+    HEROKU: process.env.HEROKU ||false,
+    API_KEY: process.env.HEROKU_API_KEY === undefined ? '1abfce1e-1bee-4334-9f6c-f4c1cb1cafab' : process.env.HEROKU_API_KEY,
+    APP_NAME: process.env.HEROKU_APP_NAME === undefined ? 'zeropgg' : process.env.HEROKU_APP_NAME
+},
+  VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
+  LANG: process.env.THEME|| 'SECKTOR',
+  WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE
 };
 
 
